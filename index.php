f4405e2510e8893815ec89500710da9cd58e1956
BrunoGottert <brunogottert16@gmail.com>
2020-03-10 21:59:13 -0300

    pageadmin create

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -13,6 +14,13 @@ $app->get('/', function() {
 	$page->setTpl("index");	
 	
 });
+
+$app->get('/admin', function() {
+	$page = new PageAdmin ();
+	$page->setTpl("index");	
+	
+});
+
 
 $app->run();
 
