4046098f40edd04a6304eff1eab0ad78795f1a60
BrunoGottert <brunogottert16@gmail.com>
2020-04-11 17:17:42 -0300

    add cart

--- site.php
+++ site.php
@@ -3,6 +3,7 @@
 use \Hcode\Page;   
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
 $app->get('/', function() {
 	$products = Product:: listAll();
@@ -30,6 +31,29 @@ $app->get("/categories/:idcategory", function($idcategory){
 		'products'=>$pagination["data"],
 		'pages'=>$pages
 	]);	
+});
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+	]);
+
+});	
+
+$app->get("/cart", function(){
+$cart = Cart::getFromSession();
+$page = new Page();
+$page->setTpl("cart");
+
+
 });
 	
 
--- vendor/hcodebr/php-classes/src/Model/user.php
+++ vendor/hcodebr/php-classes/src/Model/user.php
@@ -9,6 +9,40 @@ class User extends Model {
   const SESSION = "User";
   const SECRET = "HcodePhp7_Secret";
   const SECRET_IV = "HcodePhp7_Secret_IV";
+
+  public static function getFromSession(){
+    $user = new User();
+    if (isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0) {
+     
+      $user->setData($_SESSION[User::SESSION]);
+      }
+      return $user;
+  }
+
+  public static function checkLogin($inadmin = true){
+    if (
+    
+        !isset($_SESSION[User::SESSION])
+        ||
+        !$_SESSION[User::SESSION]
+        ||
+        !(int)$_SESSION[User::SESSION]["iduser"] > 0
+    ){
+      //não está logado
+      return false;
+    } else { 
+      if ($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin'] === true){
+        return true;
+
+      } else if($inadmin === false){
+        return true;
+      } else {
+        return false;
+      }
+
+    }
+    }
+
   public static function login($login, $password)
 {
 
@@ -41,15 +75,7 @@ if (password_verify($password, $data["despassword"]) === true)
 }
   public static function verifyLogin($inadmin = true)
 {
-  if (
-  !isset($_SESSION[User::SESSION])
-  ||
-  !$_SESSION[User::SESSION]
-  ||
-  !(int)$_SESSION[User::SESSION]["iduser"] > 0
-  ||
-  (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
-  ) {
+  if (User::checkLogin($inadmin)) {
   header("Location: /admin/login");
   exit;
 
